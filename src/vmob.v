79c21a3111fac10ee3dc7a5e4e0b00ab05ab9971
Nedim <nedx45@gmail.com>
2021-04-10 15:49:18 +0200

    check for required tools and check for supported OS

--- src/vmob.v
+++ src/vmob.v
@@ -1,12 +1,18 @@
-//Written by: @nedimf 04/04/2021
+/*
+* Tool originally written by @nedimcodes (nedimf) 04/2021 
+* Vmob is CLI tool that cross-compiles V module for use in iOS/Android trough C layer
+* Read documentation to understand how this tool works
+* MIT license 2021
+*/
 module main
 
 import os
+import term
 import cli { Command, Flag }
 
 const (
-	ios_sdk_path           = '/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS.sdk'
-	ios_simulator_sdk_path = '/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iphonesimulator14.4.sdk'
+	ios_sdk_path           = '/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS.sdk' // universal path to iPhoneOS sdk
+	ios_simulator_sdk_path = '/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iphonesimulator' // universal part to iphonesimulator sdk
 )
 
 fn main() {
@@ -64,13 +70,26 @@ fn main() {
 		abbrev: 'o'
 		description: 'Universal library name'
 	})
-	app.add_command(cmd_apple_ios_iphone)
-	app.add_command(cmd_apple_ios_simulator)
-	app.add_command(cmd_apple_lipo)
+	running := os.uname().sysname
+	match running {
+		'Darwin' {
+			app.add_command(cmd_apple_ios_iphone)
+			app.add_command(cmd_apple_ios_simulator)
+			app.add_command(cmd_apple_lipo)
+		}
+		else {
+			println(term.fail_message('At the moment vmob is only supported on MacOS with Xcode tools installed. This will change once Android support is implemented'))
+		}
+	}
 	app.setup()
 	app.parse(os.args)
+
+	if !toolset_check() {
+		exit(1)
+	}
 }
 
+// apple_ios_iphone creates arm64 static library
 fn apple_ios_iphone(cmd Command) ? {
 	skip_unused := cmd.flags.get_bool('skip-unused') ?
 	bitcode := cmd.flags.get_bool('disable-bitcode') ?
@@ -109,37 +128,41 @@ fn apple_ios_iphone(cmd Command) ? {
 	}
 }
 
+// apple_ios_simulator creates x86_64 static library
 fn apple_ios_simulator(cmd Command) ? {
 	skip_unused := cmd.flags.get_bool('skip-unused') ?
 	module_name := cmd.args[0]
-		mut out := os.Result{
-			exit_code: 0
-			output: ''
-		}
-		if !skip_unused {
-			out = os.execute('v -o $module_name-x86_64.c ${module_name}.v -shared vlib')
-		} else {
-			out = os.execute('v -o $module_name-x86_64.c ${module_name}.v -shared vlib -skip-unused')
-		}
-		out = os.execute('gcc -c $module_name-x86_64.c -o $module_name-x86_64.a -target x86_64-apple-ios-simulator -isysroot $ios_simulator_sdk_path')
-		otool := otool_check(module_name, false)
+	mut out := os.Result{
+		exit_code: 0
+		output: ''
+	}
+	out = os.execute("xcodebuild -version -sdk iphoneos | grep SDKVersion | cut -f2 -d ':' | tr -d '[[:space:]]'")
+	simulator_sdk := '$ios_simulator_sdk_path${out.output}.sdk'
+	if !skip_unused {
+		out = os.execute('v -o $module_name-x86_64.c ${module_name}.v -shared vlib')
+	} else {
+		out = os.execute('v -o $module_name-x86_64.c ${module_name}.v -shared vlib -skip-unused')
+	}
+	out = os.execute('gcc -c $module_name-x86_64.c -o $module_name-x86_64.a -target x86_64-apple-ios-simulator -isysroot $simulator_sdk')
+	otool := otool_check(module_name, false)
+	if out.exit_code < 1 {
+		println('iOS iPhone Simulator(x86_64) static library crated.')
+		out = os.execute('lipo -info $module_name-x86_64.a')
 		if out.exit_code < 1 {
-			println('iOS iPhone Simulator(x86_64) static library crated.')
-			out = os.execute('lipo -info $module_name-x86_64.a')
-			if out.exit_code < 1 {
-				println('Lipo check passed ✅')
-				if otool {
-					println('Otool check passed ✅')
-				} else {
-					println('Otool check failed: ❌')
-				}
+			println('Lipo check passed ✅')
+			if otool {
+				println('Otool check passed ✅')
 			} else {
-				println('Lipo check failed: ❌')
-				println('Lipo check: \n$out.output')
+				println('Otool check failed: ❌')
 			}
-		} 
+		} else {
+			println('Lipo check failed: ❌')
+			println('Lipo check: \n$out.output')
+		}
+	}
 }
 
+// apple_lipo creates universal static library by combining both arm64 and x86_64
 fn apple_lipo(cmd Command) ? {
 	output := cmd.flags.get_string('output') or { panic('Output flag is required') }
 	module_arm64 := cmd.args[0]
@@ -163,6 +186,7 @@ fn apple_lipo(cmd Command) ? {
 	}
 }
 
+// otool_check checks created static library to see if sdk is present in generated .a library
 fn otool_check(module_name string, is_iphone bool) bool {
 	mut out := os.Result{
 		exit_code: 0
@@ -173,11 +197,34 @@ fn otool_check(module_name string, is_iphone bool) bool {
 		if out.exit_code < 1 {
 			return true
 		}
-	}else{
+	} else {
 		out = os.execute('otool -lv $module_name-x86_64.a | grep sdk')
 		if out.exit_code < 1 {
 			return true
 		}
 	}
 	return false
+}
+
+// toolset_check checks for presents of needed tools, such as [gcc, otool, xcodebuild, xcode-select]
+fn toolset_check() bool {
+	tools_to_check := ['gcc --version', 'otool --version', 'xcodebuild -version',
+		'xcode-select --version',
+	]
+	mut out := os.Result{
+		exit_code: 0
+		output: ''
+	}
+	for tool in tools_to_check {
+		out = os.execute(tool)
+		if out.exit_code > 0 {
+			println('\n')
+			println(term.fail_message('Required tool missing'))
+			println('Missing tool: $tool')
+			println('Error message: $out.output')
+			return false
+		} else {
+			return true
+		}
+	}
 }
